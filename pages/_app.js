bc64aa6c8f499da8587dc4b7d1e8fd6d2c71635c
natanaelcaesario <natanaelcaesario09@gmail.com>
2021-09-11 12:03:23 +0700

    Next Authentication Project Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import { Provider } from "next-auth/cleint";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider session={pageProps.session}>
+      <Component nt {...pageProps} />;
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
